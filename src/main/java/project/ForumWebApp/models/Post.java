5d414a8feac9d5ce5562a445d6e86a193f1e5a3f
DobromirMD <122624182+DobromirMD@users.noreply.github.com>
2024-07-22 18:11:34 +0300

    Created_at fixed for Post
    Fixed the issue of created_at field being set to null at creation for Post entity.

--- src/main/java/project/ForumWebApp/models/Post.java
+++ src/main/java/project/ForumWebApp/models/Post.java
@@ -1,21 +1,11 @@
 package project.ForumWebApp.models;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
@@ -23,6 +13,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Data
 @NoArgsConstructor
@@ -40,8 +31,9 @@ public class Post {
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", referencedColumnName = "id")
     private ApplicationUser user;
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
     private Instant createdDate;
-
     @JsonManagedReference
     @OneToMany(fetch = FetchType.EAGER)
     @JoinColumn(name = "post_id")
