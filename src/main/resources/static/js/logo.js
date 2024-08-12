b5211e9f16fdce3041304afc1277cb3800d04290
svetoslavst <svetoslav.stoyanov66@gmail.com>
2024-08-12 14:43:40 +0300

    page logo addition

---
+++ src/main/resources/static/js/logo.js
@@ -0,0 +1,8 @@
+// Select the image element
+const image = document.getElementById('logo');
+
+// Add a click event listener to the image
+image.addEventListener('click', function() {
+    // Navigate to the /home route
+    window.location.href = '/home';
+});
